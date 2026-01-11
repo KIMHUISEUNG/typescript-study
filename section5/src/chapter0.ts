d4cab47c8e68177399e11ca5317a79d2be1101a7
hatso <mmorpg3636@naver.com>
2026-01-11 22:42:16 +0900

    section5 complete

---
+++ section5/src/chapter0.ts
@@ -0,0 +1,21 @@
+/**
+ * 인터페이스
+ * readonly 읽기 전용 사용 가능, ? 선택적 프로퍼티 사용가능
+ */
+
+interface Person {
+  readonly name: string;
+  age?: number;
+  sayHi: () => void;
+  //함수 시그니처
+  //sayHi(): void;
+  //sayHi(a: number, b: number): void;
+}
+//메서드를 오버로드해서 사용하고 싶다면 함수 시그니처를 사용해야한다.
+
+const person: Person = {
+  name: "김희성",
+  sayHi: function () {
+    console.log("Hi");
+  },
+};
---
+++ section5/src/chapter1.ts
@@ -0,0 +1,8 @@
+/**
+ * 인터페이스의 확장
+ */
+
+interface Animal {
+  name: string;
+  age: number;
+}
